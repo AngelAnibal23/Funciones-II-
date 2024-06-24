0a7c24e9b05bf58fb5d04eb6fa21bb551872214e
Guts <aamamanica@unjbg.edu.pe>
2024-06-23 23:32:05 -0500

    Agregando restricciones de caracteres a los valores numericos

--- Ejercicio5-funciones.cpp
+++ Ejercicio5-funciones.cpp
@@ -28,28 +28,54 @@ int main() {
     int a, b, c;
     double raiz1, raiz2;
     bool solucionesReales;
+    char letra; 
      
     cout << "Digite el valor de a: ";
     cin >> a;
+    
+    if(a==letra){
+    	cout<<endl<<"El dato ingresado no es valido. "<<endl;
+    	cout<<"----------ERROR-----------";
+    	
+    	return 1; 
+    }
+    
     cout << "Digite el valor de b: ";
     cin >> b;
+    
+     if(b==letra){
+    	cout<<endl<<"El dato ingresado no es valido. "<<endl;
+    	cout<<"----------ERROR-----------";
+    	
+    	return 2; 
+    }
+    
     cout << "Digite el valor de c: ";
     cin >> c;
-
+    
+     if(c==letra){
+    	cout<<endl<<"El dato ingresado no es valido. "<<endl;
+    	cout<<"----------ERROR-----------";
+    	
+    	return 3; 
+    }
+    
     if (a == 0) {
         cout << endl << "Los datos ingresados no representan una ecuacion cuadratica.";
-        return 1;
+        return 4;
     }
 
     calcularRaices(a, b, c, raiz1, raiz2, solucionesReales);
     
     if(solucionesReales){
-        cout<< endl << "Las raices de la ecuación cuadratica son: X1 = " << raiz1 << "  X2 = " << raiz2 << endl;
+        cout<< endl << "Las raices de la ecuacion cuadratica son: X1 = " << raiz1 << "  X2 = " << raiz2 << endl;
     }
     else{
     	cout<< endl << "Tiene raices complejas no reales. "; 
     }
-
+	
+	
+	
     return 0;
 }
 
